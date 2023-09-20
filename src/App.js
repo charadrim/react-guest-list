0b96c9e368131ee0ee30f6376c27b0ffe8dbd14b
Chara Drimiskianaki <charadrim@yahoo.gr>
2023-09-20 22:24:31 +0300

    Update name and last name input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import './App.css';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import GuestForm from './GuestForm';
+import GuestList from './GuestList';
 
 export default function App() {
+  const [guests, setGuests] = useState([]);
+  const handleInputChange = (event) => {
+    setGuests(event.target.value);
+  };
+
+  const handleBtnClick = () => {
+    console.log(guests);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Guest List App</h1>
+      <div>
+        <label htmlFor="first-name">First Name:</label>
+        <input
+          type="text"
+          id="first-name"
+          name="name"
+          value={guests}
+          onChange={handleInputChange}
+        ></input>
+        <br />
+        <label htmlFor="last-name">Last Name:</label>
+        <input type="text" id="last-name" name="last"></input>
+        <br />
+        <button onClick={handleBtnClick}>Return</button>
+      </div>
     </div>
   );
 }
